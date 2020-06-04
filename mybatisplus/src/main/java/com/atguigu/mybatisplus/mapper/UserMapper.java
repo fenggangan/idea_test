8cb8acca6fa15fd56a69bbee95f69510e46cad9d
“fenggangan <363999111@qq.com>
2020-06-04 11:54:19 +0900

    2020年6月4日11:54:16

---
+++ mybatisplus/src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.atguigu.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.atguigu.mybatisplus.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+
+}
